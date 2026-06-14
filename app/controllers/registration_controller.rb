2084cce970c52b62e63348444f7e921e36f60dda
harishtpj <harishtpj@hotmail.com>
2026-06-14 01:43:42 +0000

    Updated UI redirection

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationController < InertiaController
   allow_unauthenticated_access
-  before_action :redirect_authenticated_user, only: [:new]
+  before_action :redirect_authenticated_user, only: [ :new ]
 
   def new
     render inertia: "Registrations/New"
@@ -10,7 +10,7 @@ class RegistrationController < InertiaController
     user = User.new(params.permit(:email_address, :password, :password_confirmation))
     if user.save
       start_new_session_for user
-      redirect_to root_path, notice: "Welcome!"
+      redirect_to dashboard_path, notice: "Welcome!"
     else
       redirect_to signup_path, alert: "Sign up failed: #{user.errors.full_messages.join(', ')}"
     end
--- test/controllers/registration_controller_test.rb
+++ test/controllers/registration_controller_test.rb
@@ -8,7 +8,7 @@ class RegistrationControllerTest < ActionDispatch::IntegrationTest
 
   test "should get create" do
     post signup_path, params: { email_address: "test@example.com", password: "password", password_confirmation: "password" }
-    assert_redirected_to root_path
+    assert_redirected_to dashboard_path
   end
 
   test "should redirect to root if already authenticated" do
